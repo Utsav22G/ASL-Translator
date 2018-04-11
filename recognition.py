92e7d1f96359a6037d4c349474b67638091e8d29
isaacvandor <isaacvandor@gmail.com>
2018-04-10 22:48:23 -0400

    Improved the recognition functionality...works best against high contrast simple backgrounds

--- recognition.py
+++ recognition.py
@@ -6,13 +6,14 @@ import math
 # parameters
 capture_region_x=0.5  # roi x start point
 capture_region_y=0.8  # roi y start point
-threshold = 60  #  threshold
-blur_value = 41  # GaussianBlur parameter
+threshold = 70  #  threshold
+blur_value = 5  # GaussianBlur parameter
 bgSubThreshold = 50
 
 # variables
 isBgCaptured = 0   # bool, whether the background captured
 triggerSwitch = False  # In case you wanna use virtual keyboard
+
 
 def printThreshold(thr):
     print("! Threshold changed to: "+str(thr))
@@ -21,13 +22,14 @@ def printThreshold(thr):
 def removeBG(videostream):
     fgmask = bgModel.apply(videostream)
     kernel = np.ones((3, 3), np.uint8)
-    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    fgmask = cv2.erode(fgmask, kernel, iterations=5)
     result = cv2.bitwise_and(videostream, videostream, mask=fgmask)
     return result
 
 
 def findFingers(result,drawing):  # -> finished bool, count: finger count
     #  convexity defect
+    result = cv2.approxPolyDP(result,0.01*cv2.arcLength(result,True),True)
     hull = cv2.convexHull(result, returnPoints=False)
     if len(hull) > 3:
         defects = cv2.convexityDefects(result, hull)
@@ -38,13 +40,15 @@ def findFingers(result,drawing):  # -> finished bool, count: finger count
                 start = tuple(result[s][0])
                 end = tuple(result[e][0])
                 far = tuple(result[f][0])
-                a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-                b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-                c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
-                if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
-                    count += 1
-                    cv2.circle(drawing, far, 8, [211, 84, 0], -1)
+                #a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+                #b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+                #c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+                #angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
+                #if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
+                #    count += 1
+                dist = cv2.pointPolygonTest(result,center,True)
+                cv2.line(img,start,end,[0,255,0],2)
+                cv2.circle(drawing, far, 8, [211, 84, 0], -1)
             return True, count
     return False, 0
 
@@ -69,35 +73,43 @@ while camera.isOpened():
 
     #  Main operation
     if isBgCaptured == 1:  # Only runs once background is captured
+        img = videostream
         img = removeBG(videostream)
-        img = img[0:int(capture_region_y * videostream.shape[0]),
-                    int(capture_region_x * videostream.shape[1]):videostream.shape[1]]  # clip the ROI
+        #img = img[0:int(capture_region_y * videostream.shape[0]),
+        #            int(capture_region_x * videostream.shape[1]):videostream.shape[1]]  # clip the ROI
         #cv2.imshow('mask', img) #show mask image
 
         # convert the image into binary image
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (blur_value, blur_value), 0)
-        #cv2.imshow('blurred', blur) # show blur image
-        ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        #cv2.imshow('binary', thresh) # show binary image
+        cv2.imshow('blurred', blur) # show blur image
+        ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        cv2.imshow('binary', thresh) # show binary image
 
 
         # get the coutours
         new_threshold = copy.deepcopy(thresh)
-        img2,contours, hierarchy = cv2.findContours(new_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        length = len(contours)
+        _, contours, hierarchy = cv2.findContours(new_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
         maxArea = -1
-        if length > 0:
-            for i in range(length):  # find the biggest contour by area
-                tmp = contours[i]
-                area = cv2.contourArea(tmp)
-                if area > maxArea:
-                    maxArea = area
-                    ci = i
+
+        for i in range(len(contours)):  # find the biggest contour by area
+            tmp = contours[i]
+            area = cv2.contourArea(tmp)
+            if area > maxArea:
+                max_area = area
+                ci = i
 
             result = contours[ci]
             hull = cv2.convexHull(result)
             drawing = np.zeros(img.shape, np.uint8)
+            moments = cv2.moments(result)
+            if moments['m00']!=0:
+                        cx = int(moments['m10']/moments['m00']) # cx = M10/M00
+                        cy = int(moments['m01']/moments['m00']) # cy = M01/M00
+
+            center = (cx, cy)
+            cv2.circle(img,center,5,[0,0,255],2)
             cv2.drawContours(drawing, [result], 0, (0, 255, 0), 2)
             cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
 
