ecf82a95d6d3d5f8214e6f3d9b7c38bce6c3d3c2
isaacvandor <isaacvandor@gmail.com>
2018-05-05 15:20:58 -0400

    Added ability to put any image into predict.py (still requires manual input of image name)

--- predict.py
+++ predict.py
@@ -1,10 +1,15 @@
 from cv2 import imread
 from keras.models import load_model
 import numpy as np
+from PIL import Image
 
 model = load_model('model.h5')
 
-img = imread('RawImages/Y_1.jpg') #try converting into a numpy array
+
+img = Image.open('Data/OutputData/scaledoutput.jpg') #try converting into a numpy array
+img = img.resize((200,200), resample=0)
+img = img.save('Data/OutputData/resultingresizedimage1.jpg')
+img = imread('Data/OutputData/resultingresizedimage1.jpg')
 img = img.astype(np.float32)/255.0
 img = img[:,:,::-1]
 result = model.predict(np.expand_dims(img, axis=0))
